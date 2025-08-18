1489e195e74d568670c4e3f70a573ccebf1c71ea
Ryan Bendall <139355026+RB200468@users.noreply.github.com>
2025-08-18 17:05:51 +0100

    add initital framework

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -5,10 +5,11 @@ type Props = {}
 function Header({}: Props) {
   return (
     <div
-        className="bg-custom-white text-black d-flex align-items-center justify-content-center"
+        className="bg-custom-white text-black d-flex align-items-center justify-content-between border"
         style={{ flex: "0 0 10%" }}
     >
-        Header
+        <div className="d-flex border">Sidebar Button</div>
+        <div className="d-flex border">Profile/Menu</div>
     </div>
   )
 }
--- client/src/components/Sidebar.tsx
+++ client/src/components/Sidebar.tsx
@@ -4,8 +4,21 @@ type Props = {}
 
 function Sidebar({}: Props) {
   return (
-    <div className="col-2 bg-custom-white text-black d-flex justify-content-center align-items-center">
-        Sidebar 
+    <div className="col-2 bg-custom-white text-black d-flex justify-content-center align-items-center g-0">
+        <div className="row h-100 w-100 flex-column border">
+            <div
+                className="d-flex border"
+                style={{ flex: "0 0 10%" }}
+            >
+                Logo
+            </div>
+            <div
+                className="d-flex"
+                style={{ flex: "1 0 90%" }}
+            >
+                Menu List
+            </div>
+        </div> 
     </div>
   )
 }
