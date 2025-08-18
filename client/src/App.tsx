8b8578d37138e398af7c867e7dd8a16a2aa48e07
Ryan Bendall <139355026+RB200468@users.noreply.github.com>
2025-08-18 14:37:34 +0100

    initial font-end framework

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,35 +1,36 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './utilities.css'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App: React.FC = () => {
+  const [counter, setCounter] = useState<number>(0)
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="container-fluid vh-100 vw-100">
+      <div className="row h-100">
+        <div className="col-2 bg-custom-white text-black d-flex justify-content-center align-items-center">
+          Sidebar
+        </div>
+        <div className="col-10 bg-danger text-white d-flex justify-content-center align-items-center g-0">
+          <div className="row h-100 w-100 flex-column">
+            <div
+              className="bg-custom-white text-black d-flex align-items-center justify-content-center"
+              style={{ flex: "0 0 10%" }}
+            >
+              Header
+            </div>
+            <div
+              className="bg-custom-grey d-flex align-items-center justify-content-center"
+              style={{ flex: "1 0 90%" }}
+            >
+              Content
+            </div>
+          </div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    </div>
+  );
+};
 
-export default App
+export default App;
+
