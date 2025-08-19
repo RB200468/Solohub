ac9ce07bddfbdfc211eadab0b11e32b58db5861f
Ryan Bendall <139355026+RB200468@users.noreply.github.com>
2025-08-19 15:38:41 +0100

    add axios and GET request

--- client/src/components/Content.tsx
+++ client/src/components/Content.tsx
@@ -1,16 +1,18 @@
 import React from 'react'
+import UsersList from './UsersList'
 
 type Props = {}
 
 function Content({}: Props) {
-  return (
-    <div
-        className="bg-custom-grey d-flex align-items-center justify-content-center"
-        style={{ flex: "1 0 90%", borderRadius: "20px", transform: "scale(0.98)", transformOrigin: "center"}}
-    >
-        Content
-    </div>
-  )
+
+    return (
+        <div
+            className="bg-custom-grey d-flex align-items-start justify-content-evenly p-3 flex-wrap"
+            style={{ flex: "1 0 90%", borderRadius: "20px", transform: "scale(0.98)", transformOrigin: "center"}}
+        >
+            <UsersList/>
+        </div>
+    )
 }
 
 export default Content
---
+++ client/src/components/UsersList.tsx
@@ -0,0 +1,57 @@
+import React, {useState, useEffect} from 'react'
+import axios from "axios"
+
+type Props = {}
+
+interface User {
+    id: number;
+    name: string;
+    email: string;
+    password: string;
+    createdAt: string;
+}
+
+const API_URL = import.meta.env.VITE_API_URL;
+
+function UsersList({}: Props) {
+    const [users, setUsers] = useState<User[]>([]);
+    const [loading, setLoading] = useState<boolean>(true);
+    const [error, setError] = useState<string | null>(null);
+    
+    useEffect(() => {
+        const fetchUsers = async () => {
+            try {
+                const res = await axios.get<User[]>(`${API_URL}/user/all`);
+                setUsers(res.data);
+            } catch (err) {
+                setError("Failed to fetch users");
+            } finally {
+                setLoading(false);
+            }
+        };
+
+        fetchUsers();
+    }, []);
+
+    if (loading) return <p>loading...</p>;
+    if (error) return <p>{error}</p>;
+
+    return (
+        <>
+            {users.map((user)=>(
+                <div className="card" style={{ width: "18rem" }}>
+                    <div className="card-header">
+                        <h3>{user.name}</h3>
+                    </div>
+                    <ul className="list-group list-group-flush">
+                        <li className="list-group-item">Email: {user.email}</li>
+                        <li className="list-group-item">Password: {user.password}</li>
+                        <li className="list-group-item">CreatedAt: {user.createdAt}</li>
+                    </ul>
+                </div>
+            ))}
+        </>
+    )
+}
+
+export default UsersList
